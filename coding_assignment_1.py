d6c8329d7bd5326a5826806e29c3fd769aac6b3c
grace-siegwald <loganrsilvers@gmail.com>
2026-02-18 08:49:53 -0600

    code?

---
+++ coding_assignment_1.py
@@ -0,0 +1,5 @@
+def CalculateFrequency(fund_frequency, num_harmonics):
+    # Making an empty list to store the harmonics that we will return!
+    harmonics = []
+    for i in range(1, num_harmonics + 1):
+        
