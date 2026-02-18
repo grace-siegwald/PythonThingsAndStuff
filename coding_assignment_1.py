709beb5df00f5215fa8393b9974d3134a2572aa4
grace-siegwald <siegwald@colum.edu>
2026-02-18 11:11:05 -0600

    AHHHHHH

--- coding_assignment_1.py
+++ coding_assignment_1.py
@@ -1,13 +1,8 @@
-# def CalculateFrequency(fund_frequency, num_harmonics):
-#     # Making an empty list to store the harmonics that we will return!
-#     harmonics = []
-#     for i in range(1, num_harmonics + 1):
-#         # Calculate the new frequency!
-#         new_freq = fund_frequency * i
-#         harmonics.append(new_freq)
-#     return harmonics
-
-
-n= int(input("Enter a number "))
-for i in range(1, n):
-    print(i*(n-1))
+def CalculateFrequency(fund_frequency, num_harmonics):
+    # Making an empty list to store the harmonics that we will return!
+    harmonics = []
+    for i in range(1, num_harmonics + 1):
+        # Calculate the new frequency!
+        new_freq = fund_frequency * i
+        harmonics.append(new_freq)
+    return harmonics
